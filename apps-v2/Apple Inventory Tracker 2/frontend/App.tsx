ec605a68fe6c56a845d65880304cc08b98a085f9
Retool Admin <admin@seed.retool.com>
2026-06-03 06:34:19 +0000

    Manually rewriting history

--- apps-v2/Apple Inventory Tracker 2/frontend/App.tsx
+++ apps-v2/Apple Inventory Tracker 2/frontend/App.tsx
@@ -7,7 +7,7 @@ export function CenterOverlay() {
     <div style={{ position: 'absolute', inset: 0, zIndex: 1, pointerEvents: 'none' }}>
       <div
       >
-test
+      testing
       </div>
     </div>
   )
