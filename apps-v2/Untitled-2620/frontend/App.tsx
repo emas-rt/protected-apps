b5d2b5bac9217d40147df8c2cfd6ee6c79c15640
Erin Masatsugu <emas@retool.com>
2026-07-31 08:57:59 -0700

    Try again

--- apps-v2/Untitled-2620/frontend/App.tsx
+++ apps-v2/Untitled-2620/frontend/App.tsx
@@ -3,7 +3,7 @@
 
 export default function App() {
   return (
- <div>{__MY_APP_VERSION__}</div>
+ <div>{__MY_APP_VERSION__ ?? 'no app version'}</div>
   )
 }
 /* eslint-enable react-perf/jsx-no-new-object-as-prop */
